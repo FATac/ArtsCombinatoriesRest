58b62b7afe4cbf396369030465fdc812e637366a
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-06-17 13:50:52 +0200

    Adjusted id length

--- sql/generate.sql
+++ sql/generate.sql
@@ -1,7 +1,7 @@
 -- Base script which creates necessary data structure for managing users, rights and uploaded media and objects
 -- Suitable (only?) for Openlink Virtuoso sql implementation
 
--- DROP TABLE db.dba._user;
+DROP TABLE db.dba._user;
 CREATE TABLE db.dba._user (
 	SID INT IDENTITY,
 	userName VARCHAR(30),
@@ -9,18 +9,18 @@ CREATE TABLE db.dba._user (
 	userType INT
 );
 
---DROP TABLE db.dba._media;
+DROP TABLE db.dba._media;
 CREATE TABLE db.dba._media (
 	SID INT IDENTITY,
 	path VARCHAR(500),
-	objectId VARCHAR(30),
+	objectId VARCHAR(60),
 	moment TIMESTAMP
 )
 
--- DROP TABLE db.dba._right;
+DROP TABLE db.dba._right;
 CREATE TABLE db.dba._right (
 	SID INT IDENTITY,
-	objectId VARCHAR(30),
+	objectId VARCHAR(60),
 	rightLevel INT
 )
 
