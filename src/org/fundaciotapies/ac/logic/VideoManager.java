75a4fe5489b4d0f723188be0cf44cd921e04a4ff
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-06-27 17:31:12 +0200

    VideoManager class will provide proper media file managing in File-System

---
+++ src/org/fundaciotapies/ac/logic/VideoManager.java
@@ -0,0 +1,12 @@
+package org.fundaciotapies.ac.logic;
+
+
+public class VideoManager {
+	
+	public void putVideo(String path) { 
+		// TODO: manage video properly
+		//File f = new File(path);
+		//String[] tmp = path.split("/");
+		//f.renameTo(new File(tmp[tmp.length-1]));
+	}
+}
