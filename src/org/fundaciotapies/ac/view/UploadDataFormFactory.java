f8f4d943872ac6242198ee45d101643cc8c36abb
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-06-27 17:33:32 +0200

    Deleted class form

--- src/org/fundaciotapies/ac/view/UploadDataFormFactory.java
+++ src/org/fundaciotapies/ac/view/UploadDataFormFactory.java
@@ -3,17 +3,9 @@ package org.fundaciotapies.ac.view;
 import java.util.List;
 
 import org.fundaciotapies.ac.view.fields.GenericInput;
-import org.fundaciotapies.ac.view.fields.Select;
 import org.fundaciotapies.ac.view.forms.InsertObjectForm;
-import org.fundaciotapies.ac.view.forms.PickClassForm;
 
 public class UploadDataFormFactory {
-	
-	public static PickClassForm pickClassForm(List<String> classesList) {
-		PickClassForm form = new PickClassForm();
-		form.setClassesList(new Select("classesList", classesList, null));
-		return form;
-	}
 	
 	public static InsertObjectForm insertObjectForm(String className, List<GenericInput> inputList) {
 		InsertObjectForm form = new InsertObjectForm();
