53e3719f83730bc67c87396335ff35a82cdbbbc1
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-07-20 17:27:10 +0200

    deleted import

--- src/org/fundaciotapies/ac/rest/GetObjectLegalColor.java
+++ src/org/fundaciotapies/ac/rest/GetObjectLegalColor.java
@@ -4,7 +4,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 
 import org.fundaciotapies.ac.model.Request;
