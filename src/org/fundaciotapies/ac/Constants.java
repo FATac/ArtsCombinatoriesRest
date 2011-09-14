57a29dcb242dca23a78f3791c8250023e8e25965
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-09-14 17:51:17 +0200

    now using a proper uri

--- src/org/fundaciotapies/ac/Constants.java
+++ src/org/fundaciotapies/ac/Constants.java
@@ -3,7 +3,7 @@ package org.fundaciotapies.ac;
 
 public class Constants {
 	public static final String RDFDB_URL = "jdbc:virtuoso://localhost:1111";
-	public static final String OWL_URI_NS = "http://www.semanticweb.org/ontologies/2011/4/Ontology1304926436539.owl#";
+	public static final String OWL_URI_NS = "http://www.fundaciotapies.org/ontologies/2011/4/ac.owl#";
 	
 	public static final String FILE_DIR = "";
 	public static final String[] VIDEO_FILE_EXTENSIONS = {"dv", "mpg", "avi"};
