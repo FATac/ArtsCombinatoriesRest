23b98bc83c1c00d5423ab470e07ccc10330422ab
jordi.roig.prieto <jordi.roig.prieto@stress.ad.upcnet.es>
2011-09-23 12:08:58 +0200

    constants fixed

--- src/org/fundaciotapies/ac/Constants.java
+++ src/org/fundaciotapies/ac/Constants.java
@@ -3,12 +3,14 @@ package org.fundaciotapies.ac;
 
 public class Constants {
 	public static final String RDFDB_URL = "jdbc:virtuoso://localhost:1111";
-	public static final String OWL_URI_NS = "http://www.fundaciotapies.org/ontologies/2011/4/ac.owl#";
 	
 	public static final String FILE_DIR = "";
 	public static final String[] VIDEO_FILE_EXTENSIONS = {"dv", "mpg", "avi"};
 	
+	public static final String OWL_URI_NS = "http://www.fundaciotapies.org/ontologies/2011/4/ac.owl#";
+	public static final String RDFS_URI_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	public static final String baseURI = "http://www.artscombinatories.cat/objects/";		// This cannot be changed without updating all existing objects IDs
 	
 	public static final String[] userRoles = { "Contributor" , "Editor" , "Member" , "Reader", "Reviewer", "Site Administrator", "Manager" };
+
 }
