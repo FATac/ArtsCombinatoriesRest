cc3b959cbda23e90599290e74b7d1890386be4d4
Ariel <ariel.von.barnekow@upcnet.es>
2012-05-22 09:31:16 +0200

    Pujada de fitxers suposa que el fitxer ja existeix al destí

--- src/org/fundaciotapies/ac/rest/UploadMedia.java
+++ src/org/fundaciotapies/ac/rest/UploadMedia.java
@@ -1,6 +1,8 @@
 package org.fundaciotapies.ac.rest;
 
-import java.io.InputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.net.URLDecoder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.POST;
@@ -10,7 +12,7 @@ import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
 
 import org.apache.log4j.Logger;
-import org.fundaciotapies.ac.model.Upload;
+import org.fundaciotapies.ac.model.Upload2;
 
 @Path("/media/upload")
 public class UploadMedia {
@@ -20,8 +22,12 @@ public class UploadMedia {
 	@Produces("application/json")
 	public String uploadMedia(@Context HttpServletRequest request, @QueryParam("fn") String fn) {
 		try {
-			InputStream in = request.getInputStream();
-			return new Upload().addMediaFile(in, fn);
+			//InputStream in = request.getInputStream();
+			File file = new File(URLDecoder.decode(fn, "utf-8"));
+		    FileInputStream in = new FileInputStream(file);
+		    String[] parts = fn.split("\\/");
+			String fileNameOnly = parts[parts.length-1];
+			return new Upload2().addMediaFile(in, fileNameOnly);
 		} catch (Exception e) {
 			log.error("Error ", e);
 			return "error";
---
+++ src/org/fundaciotapies/ac/rest/UploadMediaOld.java
@@ -0,0 +1,31 @@
+package org.fundaciotapies.ac.rest;
+
+import java.io.InputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+
+import org.apache.log4j.Logger;
+import org.fundaciotapies.ac.model.Upload;
+
+@Path("/media/upload/old")
+public class UploadMediaOld {
+	private static Logger log = Logger.getLogger(UploadMediaOld.class);
+
+	@POST
+	@Produces("application/json")
+	public String uploadMedia(@Context HttpServletRequest request, @QueryParam("fn") String fn) {
+		try {
+			InputStream in = request.getInputStream();
+			return new Upload().addMediaFile(in, fn);
+		} catch (Exception e) {
+			log.error("Error ", e);
+			return "error";
+		}
+	}
+
+}
